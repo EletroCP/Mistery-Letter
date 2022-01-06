18e431f3dce1049bf08c2b384110c80d0a6d3a6c
Breno L. Lopes <brernolopes@hotmail.com>
2022-01-06 10:15:33 -0300

    Desafio 18 ok

--- script.js
+++ script.js
@@ -10,14 +10,52 @@ function criarCarta() {
     document.getElementById("carta-gerada").appendChild(element)
   }
 
+  let style = ["newspaper", "magazine1", "magazine2"]
+  let size = ["medium", "big", "reallybig"]
+  let rotate = ["rotateleft", "rotaterigth"]
+  let tilt = ["skewleft", "skewrigth"]
+
+
+
   let partes = cartaTexto.value.split(" ")
   for (let index = 0; index < partes.length; index += 1) {
     let element = document.createElement("span")
     element.innerHTML = partes[index]
+    element.classList.add(style[parseInt(Math.random() * 3)])
+    element.classList.add(size[parseInt(Math.random() * 3)])
+    element.classList.add(rotate[parseInt(Math.random() * 2)])
+    element.classList.add(tilt[parseInt(Math.random() * 2)])
+    element.style.display = "inline-block"
     document.getElementById("carta-gerada").appendChild(element)
+
   }
-
-
+  document.getElementById("carta-contador").innerHTML = partes.length
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"newspaper medium rotateleft skewleft", "newspaper medium rotateleft skewrigth", "newspaper medium rotaterigth skewleft", "newspaper midium rotaterigth skewrigth"
+"newspaper big rotateleft skewleft", "newspaper big rotaterigth skewleft", "newspaper big rotateleft skewrigth", "newspaper big rotaterigth skewrigth",
+"newspaper reallybig rotateleft skewleft", "newspaper reallybig rotaterigth skewleft", "newspaper reallybig rotateleft skewrigth", "newspaper reallybig rotaterigth skewrigth",
+
+"magazine1 medium rotateleft skewleft", "magazine1 medium rotateleft skewrigth", "magazine1 medium rotaterigth skewleft", "magazine1 midium rotaterigth skewrigth"
+"magazine1 big rotateleft skewleft", "magazine1 big rotaterigth skewleft", "magazine1 big rotateleft skewrigth", "magazine1 big rotaterigth skewrigth",
+"magazine1 reallybig rotateleft skewleft", "magazine1 reallybig rotaterigth skewleft", "magazine1 reallybig rotateleft skewrigth", "magazine1 reallybig rotaterigth skewrigth",
+
+"magazine2 medium rotateleft skewleft", "magazine2 medium rotateleft skewrigth", "magazine2 medium rotaterigth skewleft", "magazine2 midium rotaterigth skewrigth"
+"magazine2 big rotateleft skewleft", "magazine2 big rotaterigth skewleft", "magazine2 big rotateleft skewrigth", "magazine2 big rotaterigth skewrigth",
+"magazine2 reallybig rotateleft skewleft", "magazine2 reallybig rotaterigth skewleft", "magazine2 reallybig rotateleft skewrigth", "magazine2 reallybig rotaterigth skewrigth"
