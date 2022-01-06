8f1bdf75666405f310494d85c31dd24401df7c13
Breno L. Lopes <brernolopes@hotmail.com>
2022-01-06 08:04:11 -0300

    Desafio 4 e 5 ok

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function criarCarta() {
+  let cartaTexto = document.getElementById("carta-texto")
   document.querySelectorAll("span").forEach(remove =>{
     remove.remove()
   })
 
-  let partes = document.getElementById("carta-texto").value.split(" ")
+  if(cartaTexto.value == ""){
+    let element = document.createElement("span")
+    element.innerHTML = "Por favor, digite o conteúdo da carta"
+    document.getElementById("carta-gerada").appendChild(element)
+  }
+
+  let partes = cartaTexto.value.split(" ")
   for (let index = 0; index < partes.length; index += 1) {
     let element = document.createElement("span")
     element.innerHTML = partes[index]
