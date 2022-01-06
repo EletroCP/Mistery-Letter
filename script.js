bf7351c3aa57949a48b8391cc752f9c8da33c11c
Breno L. Lopes <brernolopes@hotmail.com>
2022-01-05 22:35:53 -0300

    Desafio 3 ok

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function criarCarta(){
-let partes = document.getElementById("carta-texto").value.split(" ")
-  for(let index = 0; index < partes.length; index +=1){
+function criarCarta() {
+  document.querySelectorAll("span").forEach(remove =>{
+    remove.remove()
+  })
+
+  let partes = document.getElementById("carta-texto").value.split(" ")
+  for (let index = 0; index < partes.length; index += 1) {
     let element = document.createElement("span")
     element.innerHTML = partes[index]
     document.getElementById("carta-gerada").appendChild(element)
   }
+
+
 }
+
+
